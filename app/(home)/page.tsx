3b6656ae397fb79370112e9c3db9d2496840970b
saiteja-in <vurukondasaiteja13@gmail.com>
2025-04-05 22:19:54 +0530

    home page and comments done

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import NavBar from "../_components/navbar";
 import { currentUser } from "@/lib/auth";
 import { ExtendedUser } from "@/schemas";
+import { getActiveProducts } from "@/lib/server-actions";
+import ActiveProducts from "../_components/active-products";
 
 const Home = async () => {
   const user = (await currentUser()) as ExtendedUser;
   console.log(user);
+  const activeProducts = await getActiveProducts();
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-black transition-colors duration-200">
       <NavBar />
-      
+      <div className="md:w-3/5 mx-auto py-10 px-6">
+    <ActiveProducts
+    activeProducts={activeProducts}
+    
+    />
+   </div>
     </div>
   );
 };
--- lib/server-actions.ts
+++ lib/server-actions.ts
@@ -79,3 +79,194 @@ interface ProductData {
       return null;
     }
   };
+
+
+  export const getOwnerProducts = async () => {
+    const authenticatedUser = await auth();
+  
+    if (!authenticatedUser) {
+      return [];
+    }
+  
+    const userId = authenticatedUser.user?.id;
+  
+    const products = await db.product.findMany({
+      where: {
+        userId,
+      },
+    });
+  
+    return products;
+  };
+  
+  export const commentOnProduct = async (
+    productId: string,
+    commentText: string
+  ) => {
+    try {
+      const authenticatedUser = await auth();
+  
+      if (
+        !authenticatedUser ||
+        !authenticatedUser.user ||
+        !authenticatedUser.user.id
+      ) {
+        throw new Error("User ID is missing or invalid");
+      }
+  
+      const userId = authenticatedUser.user.id;
+  
+      // Check if authenticated user has a profile picture
+      const profilePicture = authenticatedUser.user.image || ""; // Use an empty string if profile picture is undefined
+  
+      await db.comment.create({
+        data: {
+          createdAt: new Date(),
+          productId,
+          userId,
+          body: commentText,
+          profilePicture: profilePicture,
+        },
+        include: {
+          user: true,
+        },
+      });
+  
+      const productDetails = await db.product.findUnique({
+        where: {
+          id: productId,
+        },
+        select: {
+          userId: true,
+          name: true, // Include the product name in the query
+        },
+      });
+  
+      // Check if the commenter is not the owner of the product
+      // if (productDetails && productDetails.userId !== userId) {
+      //   // Notify the product owner about the comment
+      //   await db.notification.create({
+      //     data: {
+      //       userId: productDetails.userId,
+      //       body: `Commented on your product "${productDetails.name}"`,
+      //       profilePicture: profilePicture,
+      //       productId: productId,
+      //       type: "COMMENT",
+      //       status: "UNREAD",
+      //       // Ensure commentId is included here
+      //     },
+      //   });
+      // }
+    } catch (error) {
+      console.error("Error commenting on product:", error);
+      throw error;
+    }
+  };
+  
+  export const deleteComment = async (commentId: string) => {
+    try {
+      await db.comment.delete({
+        where: {
+          id: commentId,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.error("Error deleting comment:", error);
+      throw error;
+    }
+  };
+  export const getActiveProducts = async () => {
+    const products = await db.product.findMany({
+      include: {
+        categories: true,
+        images: true,
+        comments: {
+          include: {
+            user: true,
+          },
+        },
+        upvotes: {
+          include: {
+            user: true,
+          },
+        },
+      },
+      orderBy: {
+        upvotes: {
+          _count: "desc",
+        },
+      },
+    });
+  
+    return products;
+  };
+
+
+  export const upvoteProduct = async (productId: string) => {
+    try {
+      const authenticatedUser = await auth();
+  
+      if (
+        !authenticatedUser ||
+        !authenticatedUser.user ||
+        !authenticatedUser.user.id
+      ) {
+        throw new Error("User ID is missing or invalid");
+      }
+  
+      const userId = authenticatedUser.user.id;
+  
+      const upvote = await db.upvote.findFirst({
+        where: {
+          productId,
+          userId,
+        },
+      });
+  
+      const profilePicture = authenticatedUser.user.image || ""; // Use an empty string if profile picture is undefined
+  
+      if (upvote) {
+        await db.upvote.delete({
+          where: {
+            id: upvote.id,
+          },
+        });
+      } else {
+        await db.upvote.create({
+          data: {
+            productId,
+            userId,
+          },
+        });
+  
+        const productOwner = await db.product.findUnique({
+          where: {
+            id: productId,
+          },
+          select: {
+            userId: true,
+          },
+        });
+  
+        // notify the product owner about the upvote
+  
+        // if (productOwner && productOwner.userId !== userId) {
+        //   await db.notification.create({
+        //     data: {
+        //       userId: productOwner.userId,
+        //       body: `Upvoted your product`,
+        //       profilePicture: profilePicture,
+        //       productId: productId,
+        //       type: "UPVOTE",
+        //       status: "UNREAD",
+        //     },
+        //   });
+        // }
+      }
+      return true;
+    } catch (error) {
+      console.error("Error upvoting product:", error);
+      throw error;
+    }
+  };
