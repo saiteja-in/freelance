922fcbb4ecfb345ae4dceb600ff65653feba847e
saiteja-in <vurukondasaiteja13@gmail.com>
2025-04-06 09:20:28 +0530

    fix done

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -20,7 +20,7 @@ export const Header = ({
         "text-3xl font-semibold",
         font.className,
       )}>
-        App Name
+        NovaNet
       </h1>
       <p className="text-muted-foreground text-sm">
         {label}
